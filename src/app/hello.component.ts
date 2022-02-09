08e776421a8ac1fa9cc51206e7a2500dea2c4643
antonkov <antonkovsharov@gmail.com>
2022-02-09 20:30:14 +0000

    Initial commit

---
+++ src/app/hello.component.ts
@@ -0,0 +1,17 @@
+import { ContentChildren, Component, QueryList } from '@angular/core';
+import { Target } from './target.component';
+
+@Component({
+  selector: 'hello',
+  template: `<ng-content></ng-content>`,
+  styles: [`h1 { font-family: Lato; }`],
+})
+export class HelloComponent {
+  @ContentChildren(Target, { descendants: true })
+  list: QueryList<Target> | null = null;
+
+  ngAfterContentInit() {
+    console.log('hey');
+    console.log('Hello', this.list);
+  }
+}
---
+++ src/app/target.component.ts
@@ -0,0 +1,7 @@
+import { ContentChildren, Component, QueryList } from '@angular/core';
+
+@Component({
+  selector: 'target',
+  template: `Target`,
+})
+export class Target {}
